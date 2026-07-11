78a2cbdad370ab5ffbdde824e028c9fce7e6877c
rujakco <rujakco.id@gmail.com>
2026-07-11 12:21:39 +0700

    Create data/products.js

---
+++ data/products.js
@@ -0,0 +1,16 @@
+// data/products.js
+window.PRODUCTS = [
+  { 
+    id:'p_m1', name:'Rujak Segar', desc:'Kombinasi buah pilihan dengan sambal original Rujak.Co. Disiapkan seketika sesaat sebelum pengantaran untuk menjaga tekstur renyah alami.', 
+    insight: 'Kesegaran otentik buah tropis pilihan.', 
+    price:35000, container:'Thinwall 1000ml', size:'Porsi Reguler', 
+    sambal:'Sambal Kacang Original', 
+    buah:['Mangga','Nanas','Bengkoang','Jambu Citra','Kedondong'], 
+    defaultSpice:3, 
+    thumbnail:'https://dk1tnyskaoive0dn.public.blob.vercel-storage.com/rujak-segar-thumb.webp', 
+    image:'https://dk1tnyskaoive0dn.public.blob.vercel-storage.com/rujak-segar-hd.webp' 
+  },
+  // ... (lanjutkan untuk produk lainnya dengan pola yang sama)
+];
+
+window.DISTRICT_MAP = { 'bekasi barat':5, 'bekasi timur':7, /* ... dst */ };
